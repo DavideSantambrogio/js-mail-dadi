ac85ab37b110020082b464d16f80f42f6fe2909d
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2023-10-23 18:49:45 +0200

    add: style

--- Gioco dei dadi/js/script.js
+++ Gioco dei dadi/js/script.js
@@ -1,25 +1,45 @@
-// genera numeri
-let playerNumber = Math.floor(Math.random() * 6) + 1;
-let computerNumber = Math.floor(Math.random() * 6) + 1;
 
-console.log (playerNumber, computerNumber)
+// inizio processo
+document.getElementById("lancio").onclick = function() {
 
-alert (`Il tuo numero è ${playerNumber}` )
-alert (`Il numero del computer è ${computerNumber}` )
+    // messaggio iniziale
+    document.getElementById("start").style.display = "none";
+    
+    // genera numeri
+    let playerNumber = Math.floor(Math.random() * 6) + 1;
 
-// condizioni di vittoria
+    document.getElementById("my-number").innerHTML = playerNumber;
 
-if (playerNumber > computerNumber) {
-    alert ("HAI VINTO !!!")
+    let computerNumber = Math.floor(Math.random() * 6) + 1;
+
+    document.getElementById("computer-number").innerHTML = computerNumber;
+
+    console.log (playerNumber, computerNumber)
+
+    console.log (`Il tuo numero è ${playerNumber}` )
+    console.log (`Il numero del computer è ${computerNumber}` )
+
+    // condizioni di vittoria
+
+    if (playerNumber > computerNumber) {
+        console.log ("HAI VINTO !!!")
+        result = ("HAI VINTO")
+    }
+
+    else if (playerNumber < computerNumber) {
+        console.log ("HAI PERSO  !!!")
+        result = ("HAI PERSO")
+    }
+
+    else if (playerNumber = computerNumber) {        
+        console.log ("PAREGGIO !!!")
+        result = ("PAREGGIO")
+    }
+
+    // risultato
+    document.getElementById("message").style.display = "flex";
+    
+    document.getElementById("result").innerHTML = result;
+
+
 }
-
-else if (playerNumber < computerNumber) {
-    alert ("HAI PERSO  !!!")
-}
-
-else if (playerNumber = computerNumber) {
-    alert ("PAREGGIO !!!")
-}
-
-alert ("Clicca OK per riprovare")
-location.reload()
